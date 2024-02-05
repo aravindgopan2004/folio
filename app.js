c8a39878608bfee5c9bef45f624724bf1a393ef2
aravind <aravindg.csa2226@saintgits.org>
2024-02-05 20:41:35 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const app=express();
+
+
+
+app.use(express.static('./public'));
+
+
+
+
+
+
+
+app.listen(5000,()=>{
+    console.log("here")
+
+})
---
+++ public/config.js
@@ -0,0 +1,71 @@
+window.PorfolioConfig = {
+  name: 'Aravind ',
+  jobTitle: 'Backend Developer',
+  jobDescription: "Welcome to my Node.js portfolio! Explore my projects showcasing expertise in backend development. From web apps to APIs, I deliver efficient solutions with Node.js, Express.js, MongoDB, and more. Let's connect to discuss collaborations and opportunities for innovation in the digital world",
+  skills: [
+    {
+      name: 'JavaScript',
+      description: "Proficient in JavaScript for both frontend and backend development. Skilled in modern ES6+ syntax, asynchronous programming, and handling browser events. Experienced in building interactive and dynamic web applications, leveraging libraries and frameworks like React and Node.js to create scalable solutions."
+    },
+    {
+      name: 'NODEJS',
+      description: "Proficient in backend development with Node.js, leveraging its asynchronous, event-driven architecture to build scalable and efficient server-side applications. Experienced in creating RESTful APIs, handling data persistence with databases like MongoDB, and integrating with frontend technologies to deliver seamless web solutions."
+    },
+    {
+      name: 'HTML',
+      description: "Proficient in creating structured and semantic HTML markup to build the foundation of web pages. Skilled in optimizing accessibility and SEO by adhering to best practices. Experienced in implementing responsive design principles to ensure compatibility across various devices and screen sizes."
+    },
+    {
+      name: 'CSS',
+      description: 'Skilled in styling web interfaces with CSS, including layout design, color schemes, and typography. Proficient in implementing responsive and mobile-first design principles to ensure optimal user experience. Experienced in utilizing CSS preprocessors like SASS and modern frameworks such as Bootstrap for efficient styling workflows.'
+    },
+    
+  ],
+  works: [
+    {
+      name: 'Vue Stripe Checkout',
+      link: 'https://github.com/OSSPhilippines/freefolio?tab=readme-ov-file',
+      description: 'Mollit dolor tempor adipisicing non pariatur in tempor cillum do. In officia ullamco laborum ullamco voluptate. Laboris sunt quis enim Lorem laboris aliquip duis non magna excepteur voluptate. Pariatur velit aliquip officia nulla anim reprehenderit deserunt. Sint nostrud ipsum do aliquip.'
+    },
+    {
+      name: 'ToDo App',
+      link: 'https://theuselessweb.com/',
+      description: 'Esse magna anim incididunt enim sint aute consectetur ipsum id nostrud eu do. Pariatur et anim enim aliquip anim. Laboris labore incididunt labore enim nisi elit excepteur deserunt irure exercitation exercitation. Lorem sit ad laborum sit velit veniam sit nisi nostrud occaecat duis nulla duis.'
+    },
+    {
+      name: 'A Mobile Game',
+      link: 'https://theuselessweb.com/',
+      description: 'Incididunt non cupidatat deserunt ut commodo qui non do Lorem. Qui aute minim enim sit magna est consectetur incididunt. Ex est cupidatat tempor pariatur deserunt labore tempor minim laboris magna ipsum minim ea ea. Consectetur do anim laboris nulla nisi veniam sint qui.'
+    }
+  ],
+  // hobbies: [
+  //   {
+  //     name: 'Rock Climbing',
+  //     description: 'Occaecat ipsum dolor ea ad ut irure do proident excepteur consectetur duis eu culpa velit.'
+  //   },
+  //   {
+  //     name: 'Surfing',
+  //     description: 'Laboris occaecat adipisicing culpa duis aute ea excepteur quis eiusmod mollit est pariatur.'
+  //   },
+  //   {
+  //     name: 'Rubick\'s Cube',
+  //     description: 'Laborum pariatur sint labore officia enim occaecat id.'
+  //   }
+  // ],
+  contacts: {
+    email: 'aravindgopan93@gmail.com',
+    mobileNo: '812931****',
+
+    links: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/aravindgopan2004'
+      },
+      {
+        name: 'Instagram',
+        link: 'https://www.instagram.com/_aravind_gopan_/'
+      },
+      
+    ]
+  }
+};
